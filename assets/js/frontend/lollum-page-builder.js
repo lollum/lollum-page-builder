da909b9641544e890fac89b4a97b365e7478cf34
Benito Lopez <hello@lopezb.com>
2016-11-04 16:32:29 +0100

    parallax JS functions

--- assets/js/frontend/lollum-page-builder.js
+++ assets/js/frontend/lollum-page-builder.js
@@ -33,10 +33,83 @@
 
 })(jQuery, 'smartresize');
 
+/*
+This is a modified version of jQuery Parallax by Ian Lunn
+http://www.ianlunn.co.uk/ - @IanLunn
+(dual licensed under the MIT and GPL licenses)
+
+This custom version uses smartresize and requestAnimationFrame
+to improve scrolling performance
+*/
+(function( $ ){
+	var $window = $(window);
+	var windowHeight = $window.height();
+
+	$window.smartresize(function () {
+		windowHeight = $window.height();
+	});
+
+	$.fn.parallax = function(xpos, speedFactor, outerHeight) {
+		var $this = $(this);
+		var getHeight;
+		var firstTop;
+
+		//get the starting position of each element to have parallax applied to it
+		$this.each(function(){
+		    firstTop = $this.offset().top;
+		});
+
+		if (outerHeight) {
+			getHeight = function(jqo) {
+				return jqo.outerHeight(true);
+			};
+		} else {
+			getHeight = function(jqo) {
+				return jqo.height();
+			};
+		}
+
+		// setup defaults if arguments aren't specified
+		if (arguments.length < 1 || xpos === null) {
+			xpos = '50%';
+		}
+		if (arguments.length < 2 || speedFactor === null) {
+			speedFactor = 0.1;
+		}
+		if (arguments.length < 3 || outerHeight === null) {
+			outerHeight = true;
+		}
+
+		// function to be called whenever the window is scrolled or resized
+		function update(){
+			var pos = $window.scrollTop();
+
+			$this.each(function(){
+				var $element = $(this);
+				var top = $element.offset().top;
+				var height = getHeight($element);
+
+				// Check if totally above or totally below viewport
+				if (top + height < pos || top > pos + windowHeight) {
+					return;
+				}
+
+				$this.css('backgroundPosition', xpos + ' ' + Math.round((firstTop - pos) * speedFactor) + 'px');
+			});
+		}
+
+		$window.bind('scroll', function() {
+ 			window.requestAnimationFrame(update);
+ 		}).smartresize(function() { window.requestAnimationFrame(update); });
+		update();
+	};
+})(jQuery);
+
 jQuery(function($) {
 
 	'use strict';
 
+	// Full-width rows
 	function full_width_rows() {
 		var rows = $('[data-stretch="full"]');
 
@@ -70,9 +143,28 @@ jQuery(function($) {
 		});
 	}
 
-	$(window).smartresize(function() {
-		full_width_rows();
-	});
+	// Rows with the parallax effect
+	function parallax_sections() {
+		var blocks = $('.parallax-yes');
+
+		blocks.each(function() {
+			var _this = $(this);
+			var xpos = _this.data('x');
+
+			_this.parallax(xpos, 0.3);
+		});
+	}
+
+	// Init functions
 
 	full_width_rows();
+
+	$(window).load(function() {
+		parallax_sections();
+	});
+
+	$(window).smartresize(function() {
+		full_width_rows();
+		parallax_sections();
+	});
 });
