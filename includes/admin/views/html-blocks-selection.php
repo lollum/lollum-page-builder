9177fd203cadae5eac0c5e0bca05f3e143d37939
Benito Lopez <hello@lopezb.com>
2016-11-15 01:46:34 +0100

    check if the DOM extension is enabled

--- includes/admin/views/html-blocks-selection.php
+++ includes/admin/views/html-blocks-selection.php
@@ -9,10 +9,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 
-<div id="blocks-selection">
-	<ul>
-		<?php foreach ( $blocks as $block ) : ?>
-			<li><a href="#" class="block-icon" data-block="<?php echo esc_attr( $block ); ?>"><i class="<?php echo esc_attr( lpb_get_block_icon( $block ) ) ?>"></i><span class="block-name"><?php echo esc_html( lpb_get_block_name( $block ) ) ?></span><span class="block-description"><?php echo esc_html( lpb_get_block_description( $block ) ) ?></span></a></li>
-		<?php endforeach; ?>
-	</ul>
-</div>
+<?php if ( class_exists( 'DOMDocument' ) ) : ?>
+	<div id="blocks-selection">
+		<ul>
+			<?php foreach ( $blocks as $block ) : ?>
+				<li><a href="#" class="block-icon" data-block="<?php echo esc_attr( $block ); ?>"><i class="<?php echo esc_attr( lpb_get_block_icon( $block ) ) ?>"></i><span class="block-name"><?php echo esc_html( lpb_get_block_name( $block ) ) ?></span><span class="block-description"><?php echo esc_html( lpb_get_block_description( $block ) ) ?></span></a></li>
+			<?php endforeach; ?>
+		</ul>
+	</div>
+<?php endif; ?>
